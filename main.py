06fbba4b300027a64e09281273ed36bd2d8bc5d7
hugofpaiva <hugofpaiva@ua.pt>
2021-03-26 11:58:51 +0000

    Connection with real card to measure RPMs in real time

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
 import argparse
 import obd
+import time
+
+# a callback that prints every new value to the console
+def new_rpm(r):
+    print(r.value)
 
 def main(mode):
-    print(mode)
-     
+    if mode == 0:
+        pass
+    elif mode == 1:
+        ports = obd.scan_serial()      
+        print(ports) 
+        connection = obd.Async("/dev/tty.OBDLinkMX68078-STN-SPP")
+
+    connection.watch(obd.commands.RPM, callback=new_rpm)
+    connection.start()
+
+    # the callback will now be fired upon receipt of new values
+
+    time.sleep(120)
+    connection.stop()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m','--mode', help='Select either "emulator" mode or "vehicle" mode', default="emulator")
+    parser.add_argument('-m','--mode', help='Select either 0 (emulator mode) or 1 (vehicle mode)', default=0,type=int, choices=[0, 1])
     args = parser.parse_args()
     main(args.mode)
