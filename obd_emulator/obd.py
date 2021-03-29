088968694abd6ae0b0bb697612020a5f666cb970
Orlando-pt <orlando.macedo15@ua.pt>
2021-03-29 22:47:26 +0100

    comment generator (line 16 obd.py) to allow pytest to end

--- obd_emulator/obd.py
+++ obd_emulator/obd.py
@@ -13,7 +13,7 @@ class OBDEmulator:
 
     def __init__(self):
         print('Magic connection made with sucess. Ready to emulate')
-        self.response_generator = ResponseGenerator()
+        # self.response_generator = ResponseGenerator()
         self.__last_command = None
 
     def close(self):
