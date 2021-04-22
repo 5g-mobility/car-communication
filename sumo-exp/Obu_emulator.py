c52677d4a7f4a629fce734e1e8619ae3449ed59d
Miguel17297 <almeida.m@ua.pt>
2021-04-22 16:49:21 +0100

    obu_emo

--- sumo-exp/Obu_emulator.py
+++ sumo-exp/Obu_emulator.py
@@ -3,6 +3,7 @@ import time
 import traci.constants as tc
 
 import obd_emulator
+from obd_emulator import *
 
 SUMO_BINARY = "sumo"
 SUMO_CMD = [SUMO_BINARY, "-c", "osm.sumocfg"]
@@ -15,20 +16,28 @@ class Vehicle:
         self.position = position
         self.speed = speed
         self.co2Emissions = co2Emissions
+        self.update_emu()
         
-        # TODO initialize the rest of the params
 
 
-    def update(self, position, speed, co2Emissions):
+    def update(self, position, speed, co2Emissions ):
         self.position = position
         self.speed = speed
         self.co2Emissions = co2Emissions
 
-        # TODO update the rest of the params
+        self.update_emu()
 
+    def update_emu(self):
 
-    def get_light_sensor():
-        return self.obdEmulator.query(commands.LIGHT_SENSOR)
+        # Todo : aleterar a localização para geo coords
+        self.obdEmulator.update_location(self.position)
+        self.air_temperature = self.obdEmulator.query(obd_emulator.commands.AMBIENT_AIR_TEMP).value
+        self.light_sensor = self.obdEmulator.query(obd_emulator.commands.LIGHT_SENSOR).value
+        self.rain_sensor = self.obdEmulator.query(obd_emulator.commands.RAIN_SENSOR).value
+        self.fog_light = self.obdEmulator.query(obd_emulator.commands.FOG_LIGHTS).value
+
+    def __str__(self) -> str:
+        return super().__str__()
 
 
 def main():
@@ -40,26 +49,28 @@ def main():
 
                 position = traci.vehicle.getPosition(veh_id)
                 speed = traci.vehicle.getSpeed(veh_id)
-                co2Emissions = traci.vehicle.getCO2Emission(veh_id)
+                co2_emissions = traci.vehicle.getCO2Emission(veh_id)
 
                 # verify if its the first time that the car pops on the net
                 if not veh_id in obd2EmulatorMap:
                     """ if the car appears on the network for the first time
                         add it to the map
                     """
-                    obd2EmulatorMap[veh_id] = Vehicle(position, speed, co2Emissions)
+                    obd2EmulatorMap[veh_id] = Vehicle(position, speed, co2_emissions)
                 else:
                     """ if the car was already initialized
                         just update it
                     """
-                    obd2EmulatorMap[veh_id].update(coord, vel, emissions)
+                    obd2EmulatorMap[veh_id].update(position , speed, co2_emissions)
                 # se nao estiver criar instancia obd2
                 # obdEmu = Obd(a    faf, adad)
 
                 # position = traci.simulation.convertGeo(x, y)
-                print("vehicle_id ", veh_id, ": ", position, " ",  traci.vehicle.getCO2Emission(veh_id))
+                print(obd2EmulatorMap["veh_0"])
+
+                time.sleep(2)
                 # print(f"Antes do sleep:{traci.simulation.getTime()}")
-            traci.simulationStep(1)
+            traci.simulationStep()
 
             # print(traci.vehicle.getSubscriptionResults("vehID"))
 
