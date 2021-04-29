9e1193362fd8b57e135a74f87877d44e92a480ae
Orlando-pt <orlando.macedo15@ua.pt>
2021-04-29 15:51:12 +0100

    api updated

--- tests/test_response_generator.py
+++ tests/test_response_generator.py
@@ -6,7 +6,7 @@ def generator():
     return ResponseGenerator()
 
 # test update sun method
-def test_update_sun(generator):
+def test_sunrise_sunset_api(generator):
     """ fail if the api returns a code different from OK"""
 	
     generator.update_params(("52.52364029919217", "13.400111802078426"))
@@ -18,16 +18,19 @@ def test_update_sun(generator):
 
 
 # test update weather method
-def test_fog_lights(generator):
+def test_openweather_api(generator):
     """ There isn\'t any param that can be used to check the integrity of the response
         so, this test just checks if the response arrives and has the
         necessary params that allows the update
     """
-    generator.update_params(("52.52364029919217", "13.400111802078426"))
+    generator.update_params(("40.8586", "-8.6251"))
 
     response = generator.update_weather()
-    assert  response['data'][0]['vis'] is not None
-    assert  response['data'][0]['precip'] is not None
+    print(response)
+    
+    assert response['cod'] == 200
+    assert response['visibility'] is not None
+    assert response['main']['temp'] is not None
 
 # test geocoder
 # def test_geocoder(generator):
